1c43c00e4524f5224cf8982eb1e9dbc4a0a43659
Shivang Bhavesh Mewada <shivangmewada24@gmail.com>
2023-08-17 11:26:45 +0530

    few small changes

--- script.js
+++ script.js
@@ -36,9 +36,35 @@
 //         console.log(earned) ;
 //     }
 //     const res = document.getElementById('result');
+//     res.innerHTML = `Your res:${(earned/22)}`;
 // }
 
+const csebtn = document.querySelector(".CSE");
+const ccebtn = document.querySelector(".CCE");
+const ecebtn = document.querySelector(".ECE");
+const mmebtn = document.querySelector(".MME");
 
+const semselector = document.querySelector(".semselector");
+let cur_sem='Sem-1';
+semselector.addEventListener("click",function(){
+    cur_sem=semselector.value;
+    removesemEvent();
+    if (cur_sem=="Sem-2") 
+    {
+        csebtn.setAttribute("onclick","displayCSE2()");  
+        ccebtn.setAttribute("onclick","displayCCE2()");  
+        ecebtn.setAttribute("onclick","displayECE2()");  
+        mmebtn.setAttribute("onclick","displayMME2()");  
+    }
+})
+
+function removesemEvent()
+{
+    csebtn.removeAttribute("onclick");
+    ccebtn.removeAttribute("onclick");
+    ecebtn.removeAttribute("onclick");
+    mmebtn.removeAttribute("onclick");
+}
 
 function displayCSE2(){
     const cse = document.querySelector(".Branch-CSE-sem2");
@@ -85,107 +111,3 @@ function displayMME2(){
     console.log("clicked") ;
 }
 
-
-const cse = document.querySelector(".CSE") ;
-const cce = document.querySelector(".CCE") ;
-const ece = document.querySelector(".ECE") ;
-const mme = document.querySelector(".MME") ;
-
-
-
-cse.addEventListener("click", (e) => {
-    const subs = document.querySelector(".subjects");
-    const val = subs.value;
-    console.log(val);
-    console.log(e.target.innerHTML);
-    if(val === "Sem-1"){
-        console.log("sem1");
-    }
-    else if(val === "Sem-2"){
-        console.log("sem2");
-            displayCSE2();
-    }
-});
-cce.addEventListener("click", (e) => {
-    const subs = document.querySelector(".subjects");
-    const val = subs.value;
-    console.log(val);
-    console.log(e.target.innerHTML);
-    if(val === "Sem-1"){
-        console.log("sem1");
-    }
-    else if(val === "Sem-2"){
-        console.log("sem2");
-        displayCCE2();
-    }
-});
-ece.addEventListener("click", (e) => {
-    const subs = document.querySelector(".subjects");
-    const val = subs.value;
-    console.log(val);
-    console.log(e.target.innerHTML);
-    if(val === "Sem-1"){
-        console.log("sem1");
-    }
-    else if(val === "Sem-2"){
-        console.log("sem2");
-        displayECE2();
-    }
-});
-mme.addEventListener("click", (e) => {
-    const subs = document.querySelector(".subjects");
-    const val = subs.value;
-    console.log(val);
-    console.log(e.target.innerHTML);
-    if(val === "Sem-1"){
-        console.log("sem1");
-    }
-    else if(val === "Sem-2"){
-        console.log("sem2");
-        displayMME2();
-    }
-});
-
-// function clicked(e){
-    
-// }
-
-const subjects = document.querySelector(".subjects");
-const branch_btn = document.querySelector(".branch-button");
-// console.log(branch_btn.value,"  value");
-// console.log(branch_btn.innerHTML,"  inner");
-
-// subjects.addEventListener("click", (e) => {
-//     if(e.target.value === "Sem-1"){
-//         console.log("sem1");
-//     }
-
-//     if(e.target.value === "Sem-2"){
-//         console.log("sem2");
-// console.log(branch_btn.innerHTML,"  value");
-        
-//         // if(branch_btn.value === "CSE"){
-//         //     displayCSE2();
-//         // }
-
-//     }
-
-//     if(e.target.value === "Sem-3"){
-//         console.log("sem3");
-//     }
-//     if(e.target.value === "Sem-4"){
-//         console.log('sem4');
-//     }
-//     if(e.target.value === "Sem-5"){
-//         console.log('sem5');
-//     }
-//     if(e.target.value === "Sem-6"){
-//         console.log('sem6');
-//     }
-//     if(e.target.value === "Sem-7"){
-//         console.log('sem7');
-//     }
-//     if(e.target.value === "Sem-8"){
-//         console.log('sem8');
-//     }
-// });
